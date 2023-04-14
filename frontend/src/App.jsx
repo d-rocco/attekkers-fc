642f647c1e718a129ebb54d5b8bcb67b4856b03f
d-rocco <dhrocco27@gmail.com>
2023-04-13 22:29:08 -0400

    Refactored by implementing react-router-dom
    I also found that I forgot to remove the Jost font import from the tailwind.css file. Removing it saved 150-200ms of stall time!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,21 @@
-import { React, useState } from 'react'
-import './tailwind.css'
-import SearchedPlayers from './components/SearchedPlayers';
+import { React } from 'react'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Root from './routes/Root';
+import Home from './pages/Home';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+)
 
 function App() {
-  const [searchedPlayers, setSearchedPlayers] = useState([]);
 
-  const players = [
-    {
-      name: "Messi",
-      id: 1
-    },
-    {
-      name: "Ronaldo",
-      id: 2
-    },
-    {
-      name: "Neymar",
-      id: 3
-    },
-    {
-      name: "Maradona",
-      id: 4
-    },
-    {
-      name: "Zidane",
-      id: 5
-    },
-    {
-      name: "De Bruyne",
-      id: 6
-    }
-  ];
-
-  function handlePlayerSearch(e) {
-    if (e.target.value.length >= 3) {
-      const tmp = players
-        .filter(player => player.name.toLowerCase().includes(e.target.value.toLowerCase()));
-      setSearchedPlayers(tmp);
-    }
-    else {
-      setSearchedPlayers([]);
-    }
-  }
 
   return (
-    <div>
-      <input type="text" name="player-search" placeholder="Player Search..."
-        className="border-2" onChange={handlePlayerSearch}
-      />
-      <SearchedPlayers players={searchedPlayers} /> 
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import { React, useState } from 'react'
+import SearchedPlayers from '../components/SearchedPlayers';
+
+function Home() {
+  const [searchedPlayers, setSearchedPlayers] = useState([]);
+
+  const players = [
+    {
+      name: "Messi",
+      id: 1
+    },
+    {
+      name: "Ronaldo",
+      id: 2
+    },
+    {
+      name: "Neymar",
+      id: 3
+    },
+    {
+      name: "Maradona",
+      id: 4
+    },
+    {
+      name: "Zidane",
+      id: 5
+    },
+    {
+      name: "De Bruyne",
+      id: 6
+    }
+  ];
+
+  function handlePlayerSearch(e) {
+    if (e.target.value.length >= 3) {
+      const tmp = players
+        .filter(player => player.name.toLowerCase().includes(e.target.value.toLowerCase()));
+      setSearchedPlayers(tmp);
+    }
+    else {
+      setSearchedPlayers([]);
+    }
+  }
+
+  return (
+    <>
+      <input type="text" name="player-search" placeholder="Player Search..."
+        className="border-2" onChange={handlePlayerSearch}
+      />
+      <SearchedPlayers players={searchedPlayers} />
+    </>
+  )
+}
+
+export default Home;
